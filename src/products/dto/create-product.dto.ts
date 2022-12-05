a3fb4d2ea1dd08ed5a72e78660ab26532ca36af9
olion500 <olion500@daum.net>
2022-12-05 21:26:56 +0900

    feat(estimate): date type transformer

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsInt, IsString } from 'class-validator';
+import { IsString } from 'class-validator';
 
 export class CreateProductDto {
   @ApiProperty()
